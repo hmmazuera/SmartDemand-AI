d44a68d62cc2a9c63f6d9f5f320edfa7bc882ae8
Mauricio Mazuera <127618014+hmmazuera@users.noreply.github.com>
2026-09-07 13:38:03 +0100

    requirements.txt

--- app/utils.py
+++ app/utils.py
@@ -5,10 +5,11 @@ import requests
 from constanst import MODEL_PATH
 from dotenv import load_dotenv
 from prompts import build_prompt
+import json
 
 load_dotenv()
 
-API_KEY = os.getenv("OPENAI_API_KEY")
+API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 def load_model():
     model = joblib.load(MODEL_PATH)
@@ -21,18 +22,29 @@ def predict_demand(model, input_data):
     return round(prediction)
 
 def generate_explanation(features, prediction):
+
     prompt = build_prompt(features, prediction)
-    responde = requests.post(
-        'https://openrouter.ai/api/v1/chat/completions',
+
+    response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
         headers={
-            'Authorization': f'Bearer {API_KEY}',
-            'Content-Type': 'application/json',
+            "Authorization": f"Bearer {API_KEY}",
+            "Content-Type": "application/json",
         },
-        json={
-            'model': 'minstralai/mistral-7b-instruct',
-            'messages': [
-                {'role': 'user', 'content': prompt}
+        data=json.dumps({
+            "model": "openrouter/free",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
             ]
-        }
+        })
     )
-    return responde.json()['choices'][0]['message']['content']
+
+    response = response.json()
+
+    if "choices" not in response:
+        return f"OpenRouter Error: {response}"
+
+    return response["choices"][0]["message"]["content"]
